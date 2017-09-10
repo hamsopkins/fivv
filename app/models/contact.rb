29a289f41fe7836a14add30cd221fb929ba85810
Sam Hopkins <sam@samhopkins.tech>
2017-09-10 17:16:14 -0400

    update contact phone validation with regex

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -3,8 +3,10 @@ class Contact < ApplicationRecord
 	validates_presence_of :user
 	validates :name, length: { in: 2..36,
 														 message: "too short or too long" }
-	validates :phone, uniqueness: { scope: :user,
-																	message: "taken by another of your contacts" }
+	validates :phone, {uniqueness: { scope: :user,
+																	 message: "taken by another of your contacts" },
+										 format: { with: /\A[^01]\d{9}\z/,
+										 					 message: "not a valid phone number"}}
 	validates :time_zone, presence: true
 	has_many :conference_contacts
 	has_many :conferences, through: :conference_contacts

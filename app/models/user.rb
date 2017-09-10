949f741548bf401ded9388b7ce9ba2c64671f134
Sam Hopkins <sam@samhopkins.tech>
2017-09-10 17:15:33 -0400

    update user phone validation with regex

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,10 @@ class User < ApplicationRecord
 	has_secure_password
 
 	validates :name, length: {in: 2..36}
-	validates :phone, presence: true,
-										uniqueness: {message: "already taken"}
+	validates :phone, { presence: true,
+											uniqueness: {message: "already taken"},
+											format: { with: /\A[^01]\d{9}\z/,
+																message: "not a valid phone number"}}
 	validates :time_zone, presence: true	
 	has_many :contacts
 	has_many :conferences

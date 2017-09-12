7e8d84950d57debacd06de73755316de5f86a979
Sam Hopkins <sam@samhopkins.tech>
2017-09-11 22:59:06 -0400

    add logged_in? and current_user application helper methods

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+	def logged_in?
+		cookies.encrypted[:user_id] = nil if session[:user_id] == nil
+		session[:user_id] != nil
+	end
+
+	def current_user
+		logged_in? ? (User.find(session[:user_id])) : nil
+	end
 end

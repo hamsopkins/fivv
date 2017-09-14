97df60424188f16a9d4ddbadb232b1c418e9352c
Sam Hopkins <sam@samhopkins.tech>
2017-09-14 16:56:51 -0400

    schema updated automatically after running migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170914201121) do
+ActiveRecord::Schema.define(version: 20170914205100) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -61,6 +61,7 @@ ActiveRecord::Schema.define(version: 20170914201121) do
     t.boolean  "active_user",     default: false
     t.datetime "created_at",                      null: false
     t.datetime "updated_at",                      null: false
+    t.datetime "expiration"
     t.index ["phone"], name: "index_users_on_phone", using: :btree
   end
 

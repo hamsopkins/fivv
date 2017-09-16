2a311d7b92c51fb9dd551bb4a0dbc5790cfa016d
Sam Hopkins <sam@samhopkins.tech>
2017-09-16 13:40:55 -0400

    sessions controller - destroy user when expired user attempts to log in, redirect to account_expired page, add note about trial mode to new user view

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,22 +5,32 @@ class SessionsController < ApplicationController
 
 	def create
 		user = User.find_by_phone(session_params[:phone])
-		if user 
+		if user
+			if user.expiration
+				if Time.now > user.expiration
+					user.destroy
+					redirect_to 'sessions#account_expired'
+				end
+			end
 			if user.authenticate(session_params[:password])
 				session[:user_id] = user.id
 				user.active_user ? redirect_to 'users#show' : redirect_to 'users#success'
 			else
-				errors = ["Authentication failed"]
+				@errors = ["Authentication failed"]
 				render :new
 			end
 		else
-			errors = ["Authentication failed"]
+			@errors = ["Authentication failed"]
 		end
 	end
 
 	def destroy
 		session.delete(:user_id)
 		redirect_to :root
+	end
+
+	def account_expired
+		render :account_expired
 	end
 
 	private

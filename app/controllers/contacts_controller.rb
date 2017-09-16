b8d49f6c9d14f28e273d28d924fd994c85630eb9
Sam Hopkins <sam@samhopkins.tech>
2017-09-16 15:23:50 -0400

    create contacts controller (import from fivv-sandbox, minor updates) and new contact view

---
+++ app/controllers/contacts_controller.rb
@@ -0,0 +1,68 @@
+class ContactsController < ApplicationController
+	def index
+		if helpers.logged_in?
+			user = helpers.current_user
+			@contacts = Contact.where('user_id = ?', user.id).order(:name)
+		else
+			redirect_to :root
+		end
+	end
+
+	def show
+		redirect_to :root unless helpers.logged_in?
+		@contact = Contact.find_by_id(params[:id])
+		redirect_to contacts_path unless @contact
+		redirect_to :root unless @contact.user == helpers.current_user
+	end
+
+	def new
+		redirect_to :root unless helpers.logged_in?
+		@contact = Contact.new unless @contact
+	end
+
+	def create
+		redirect_to :root unless helpers.logged_in?
+		@contact = Contact.new(contact_params)
+		@contact.user = helpers.current_user
+		if @contact.save
+			redirect_to contacts_path
+		else
+			@errors = @contact.errors.full_messages
+			render :new
+		end
+	end
+
+	def edit
+		@contact = Contact.find_by_id(params[:id])
+		redirect_to :root if @contact.user != helpers.current_user
+	end
+
+	def update
+		redirect_to :root unless helpers.logged_in?
+		@contact = Contact.find_by_id(params[:id])
+		if @contact.user == helpers.current_user
+			@contact.assign_attributes(contact_params)
+			if @contact.save
+				redirect_to contact_path(@contact.id)
+			else
+				@errors = @contact.errors.full_messages
+				render :edit
+			end
+		else
+			redirect_to :root
+		end
+	end
+
+	def destroy
+		redirect_to :root unless helpers.logged_in?
+		@contact = Contact.find_by_id(params[:id])
+		@contact.destroy if belongs_to_user(@contact) if @contact
+		redirect_to 'contacts#index'
+	end
+
+
+	private
+	def contact_params
+		params.require(:contact).permit :name, :phone, :time_zone
+	end
+end

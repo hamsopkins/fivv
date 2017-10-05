c4b7b0a2302bb680117960ea6719b9eb0186a9a0
Sam Hopkins <sam@samhopkins.tech>
2017-10-05 14:28:01 -0400

    remove extraneous comma

--- db/migrate/20171005182357_add_conference_count_to_users.rb
+++ db/migrate/20171005182357_add_conference_count_to_users.rb
@@ -1,5 +1,5 @@
 class AddConferenceCountToUsers < ActiveRecord::Migration[5.0]
   def change
-  	add_column, :users, :conference_count, :integer, default: 0
+  	add_column :users, :conference_count, :integer, default: 0
   end
 end
